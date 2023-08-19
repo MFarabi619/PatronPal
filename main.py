c68ba7c869a4203670bb6a967827ff440da05327
vishnupsatish <vishnupavan.satish@gmail.com>
2023-08-19 10:28:29 -0400

    update

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, request, redirect
+import json
+
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+
+@app.route('/')
+def index():
+    return 'hi'
+
+app.run(debug=True)
