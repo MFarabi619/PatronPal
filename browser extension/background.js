7c6b5599e622c74650c04125c7f8f20e0a4de236
Mumtahin Farabi <mfarabi619@gmail.com>
2023-08-19 14:01:36 -0400

    feat: Working basic browser extension

--- browser extension/background.js
+++
@@ -1,9 +0,0 @@
-// Example: Log the URLs of all web requests
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    console.log("URL:", details.url);
-    // Check if the URL is from YouTube and track the watchtime
-  },
-  {urls: ["<all_urls>"]}, // Filter to monitor all web requests
-  ["blocking"] // Use the "blocking" permission to intercept requests
-);
