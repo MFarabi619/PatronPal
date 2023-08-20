707772dd69383089f0631dd353f1ab60a34ec7d3
vishnupsatish <vishnupavan.satish@gmail.com>
2023-08-19 23:41:14 -0400

    Start backend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
 import json
+import requests
+
+app_id = '3tjqahi5'
 
 app = Flask(
   __name__,
@@ -7,8 +10,22 @@ app = Flask(
   static_folder='static'
 )
 
+def get_kv_data(key):
+    r = requests.get(f'https://keyvalue.immanuel.co/api/KeyVal/GetValue/{app_id}/{key}')
+    return r.text
+
+
 @app.route('/')
 def dashboard():
+    all_creators = get_kv_data('allcreators').split(',')
+    with open(url_for('static', filename='data.json'), 'r') as f:
+        cjson = json.loads(f.read())
+
+    for creator in all_creators:
+        creator_data = { 'creator': creator }
+        all_ids = get_kv_data(creator).split(',')
+        print(all_ids)
+
     return render_template('dashboard.html')
 
 
